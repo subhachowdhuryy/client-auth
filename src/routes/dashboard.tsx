1399b980720af132e9716794166e87f35894a401
Subha Chowdhury <subha.chowdhury@karmicksolutions.com>
2025-05-28 16:51:48 +0530

    Dummy data AG grid shown

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,16 +1,18 @@
-import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
-import Navbar from '../components/Navbar';
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import Navbar from "../components/Navbar";
+import UsersGrid from "../components/usersGrid/UsersGrid";
 
 export default function Dashboard() {
   return (
     <>
       <Navbar />
       <SignedIn>
-        <div>
-          <br/>
-          <br/>
+        <div style={{ paddingTop: "80px" }}>
           <h1>Dashboard</h1>
           <p>Welcome! You are signed in.</p>
+          <div style={{ paddingTop: "80px" }}>
+            <UsersGrid />
+          </div>
         </div>
       </SignedIn>
       <SignedOut>
